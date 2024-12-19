a32a50e6d71eea9cc9b643e33eaf5b62c64a9d4a
Cieko <110197023+cieko@users.noreply.github.com>
2024-12-20 00:40:08 +0530

    some data and splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from 'react'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Slot, Stack } from 'expo-router'
+import { Text } from 'react-native'
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+const RootLayout = () => {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    <>
+      {/* ONE METHOD */}
+      {/* <Text>Header</Text>
+      <Slot />
+      <Text>Footer</Text> */}
+
+      {/* SECOND METHOD */}
+      {/* <Stack>
+        <Stack.Screen name='index' options={{ headerShown: false }} />
+      </Stack> */}
+    </>
+  )
 }
+
+export default RootLayout
