f4ae49d7fae6895ca8e1d8d62eefb842236ccdad
Cieko <110197023+cieko@users.noreply.github.com>
2024-12-20 01:40:27 +0530

    Folder structure modification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { Slot, Stack } from 'expo-router'
-import { Text } from 'react-native'
+import { Slot, SplashScreen, Stack } from 'expo-router'
+
+import { useFonts } from 'expo-font'
+
+import "../global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+  
+  useEffect(() => {
+    if (error) throw error;
+  
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+  
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <>
       {/* ONE METHOD */}
@@ -12,9 +42,9 @@ const RootLayout = () => {
       <Text>Footer</Text> */}
 
       {/* SECOND METHOD */}
-      {/* <Stack>
+      <Stack>
         <Stack.Screen name='index' options={{ headerShown: false }} />
-      </Stack> */}
+      </Stack>
     </>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const HomeScreen = () => {
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+
+const App = () => {
   return (
-    <View>
-      <Text>Grape</Text>
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-3xl font-pblack'>Grape!</Text>
+      <StatusBar style='auto' />
+
+      <Link href={"/profile"} className='text-primary'>Go to Profile</Link>
     </View>
   )
 }
 
-export default HomeScreen
+export default App
 
 const styles = StyleSheet.create({})
